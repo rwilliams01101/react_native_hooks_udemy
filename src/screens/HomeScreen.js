110f9a758816cf5d6b7e0ffdc1da3e79d43fe165
RAW <rwilliams01101@gmail.com>
2020-10-26 17:12:55 -0500

    end of section 4

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.text}>If it's beer, it's here.</Text>
       <Button
-        onPress={() => console.log("Button pressed")}
+        onPress={() => navigation.navigate("Components")}
         title="Go to Components Demo"
       />
-      <TouchableOpacity onPress={() => console.log("List Pressed")}>
-        <Text>Go to List Demo</Text>
-      </TouchableOpacity>
+      <Button
+        onPress={() => navigation.navigate("List")}
+        title="Go to List Demo"
+      />
     </View>
   );
 };
